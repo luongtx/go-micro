35cc2aab460f9ba3fe00a440b4cd885284192659
Luong Tran <luongtran@deputy.com>
2025-02-22 10:20:08 +0700

    update dlv script

--- broker-service/dlv.sh
+++ broker-service/dlv.sh
@@ -3,4 +3,4 @@
 # find the port id of the running container
 PID=`pgrep brokerApp`
 # attach delve to the running container
-dlv attach $PID --headless --listen=:2345 --api-version=2 --log
+dlv attach $PID --headless --listen=:2345 --accept-multiclient --api-version=2 --log
